6c19d716a55cc35fb245934db346b9b92b1ed95a
QDestTM <dimag4681@gmail.com>
2025-03-17 21:55:39 +0200

    App's root widget was implemented.

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:material_tap/const.dart';
+import 'package:material_tap/pages/home.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
+
+
+void main()
+{
+	// Setting color of bottom bottom nav-bar for mobile devices
+	final scheme = ColorScheme.fromSeed(seedColor: seedColor);
+
+	SystemChrome.setSystemUIOverlayStyle(
+		SystemUiOverlayStyle(
+			systemNavigationBarColor: scheme.secondaryFixed
+		),
+	);
+
+	// Initializing app widget
+	final Widget app = const MaterialTap();
+
+	// Starting app
+	runApp(app);
+}
+
+
+class MaterialTap extends StatelessWidget
+{
+	// ^ ----------------------------------------------------------------------------------------------------<
+
+	const MaterialTap({
+		super.key
+	});
+
+	// # ----------------------------------------------------------------------------------------------------<
+
+	@override
+	Widget build(BuildContext context)
+	{
+		final scheme = ColorScheme.fromSeed(seedColor: seedColor);
+		final theme = ThemeData(colorScheme: scheme);
+
+		return MaterialApp(
+			title: 'Material Tap', theme: theme,
+			debugShowCheckedModeBanner: false,
+
+			home: const HomePage(),
+		);
+	}
+
+	// ------------------------------------------------------------------------------------------------------<
+}
