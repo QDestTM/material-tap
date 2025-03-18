11949495c88061a02edd575fd37f1aae3a3397e4
QDestTM <dimag4681@gmail.com>
2025-03-18 23:03:50 +0200

    Removed redudant const values from "const" module.

--- lib/const.dart
+++ lib/const.dart
@@ -1,43 +1,11 @@
+import 'package:material_tap/types.dart';
 import 'package:flutter/material.dart';
 
-
-// Main seed color of whole app
+// Main seed color for whole app
 const Color seedColor = Colors.teal;
 
-// Style constants
-const iconBaseShadows = <BoxShadow>
-[
-	BoxShadow(
-		color: Color.fromARGB(124, 0, 0, 0),
-		spreadRadius: 4,
-		blurRadius: 4,
-		offset: Offset(1, 1)
-	)
-];
-
-const itemBaseShadows = <BoxShadow>
-[
-	BoxShadow(
-		color: Color.fromARGB(124, 0, 0, 0),
-		blurRadius: 3,
-		spreadRadius: 2,
-	)
-];
-
-// Constant values for sizing
-const double resourceSlotSize = 64.0;
-const double resourceLineSize = 86.0;
-
-const double resourceDisplayIconSize = 72.0;
-const double resourceDisplaySize = 100.0;
-
-const double paddingOffset = 12.0;
-
-const Duration slotAnimationTime = Duration(milliseconds: 800);
-
-
-// Constant instances
-const resourceIconsSet = <IconData>[
+// Set of selected icons for representing resources
+const resourceDataSet = <ResourceData>[
 	Icons.abc,
 	Icons.tab_sharp,
 	Icons.ac_unit,
